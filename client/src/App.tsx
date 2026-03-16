2c68d0cb76030cc8c35763160c10d49a9b78382f
ejbesana04 <ej04besana@gmail.com>
2026-03-17 00:54:01 +0800

    student info

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,30 +4,15 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  let name: string = "EJ B. Besana";
+  let age: number = 21;
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="font-mono font-bold text-2xl bg-clip-text text-transparent bg-linear-to-b from-blue-600 via-sky-500 to-purple-500">Student Information</h1>
+      <p className="text-lg font-semibold text-gray-700 mb-2">Name: {name}</p>
+      <p className="text-lg font-semibold text-gray-700 mb-2">Age: {age}</p>
     </>
   )
 }
